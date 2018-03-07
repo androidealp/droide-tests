f967ec59e638d3bcba5004a99f2a6fc5b6d6dbad
andrenext4 <andre@next4.com.br>
2018-03-07 18:12:45 -0300

    ajustes de busca incompleto

--- assets/droide-tests.js
+++ assets/droide-tests.js
@@ -4,11 +4,11 @@
  
     $.fn.DroideTests = function() {
 
-    	var jsongenerate = {};
+    	var atributos = {};
     	var $css = 'position:absolute; bottom:0; left:0; width:100%;background:rgba(169, 68, 66,0.5); color:#fff;';
 
     	var $styleElement = "<style type='text/css'>";
-    	$styleElement += ".droidetestsHover{border:2px solid #ff0000;}";
+    	$styleElement += "[droideclasshover]{border:2px solid #ff0000;}";
     	$styleElement += ".droide-menu-clique{width: 200px; position:absolute;}";
     	$styleElement += ".droide-menu-clique li{background-color: #eee;color: black;display: block;padding: 12px;text-decoration: none;}";
     	$styleElement += ".droide-menu-clique li:hover{background-color: #ccc;cursor:pointer}";
@@ -27,15 +27,21 @@
 
 	      	if(!$(this).is('[droide]'))
 	      	 {
-	      	 	e.preventDefault();
+				   e.preventDefault();
+				   
+				   var element = $(this);
 
-	      	 	positions = positionMenu($(this).offset(), e, $(this).parent());
+				   console.log("verificando elemento clicado....");
 
-				$('#droide-menu-clique').css({'top':positions.x, 'left':positions.y});	      	 	
+				   verificandoElementoClicado(element)
 
-	      	    $('#droide-menu-clique').show('slow');
+	      	 	// positions = positionMenu($(this).offset(), e, $(this).parent());
 
-	      		console.log(positions);
+				// $('#droide-menu-clique').css({'top':positions.x, 'left':positions.y});	      	 	
+
+	      	    // $('#droide-menu-clique').show('slow');
+
+	      		//console.log(positions);
 
 	      	 }
 	      		
@@ -47,8 +53,12 @@
 
 	      	 if(!$(this).is('[droide]'))
 	      	 {
-	      	 	$(this).addClass('droidetestsHover');
-	      	 	ambient.find(':selected').text();
+				   //$(this).addClass('droidetestsHover');
+
+				   $(this).attr('droideclasshover','');
+				   
+				  // $(this).css({'border':'2px solid #ff0000'});
+	      	 	//ambient.find(':selected').text();
 	      	 }
 	      		
 	      		
@@ -60,8 +70,11 @@
 
 	      	if(!$(this).is('[droide]'))
 	      	 {
-	      	 	$(this).removeClass('droidetestsHover');
+				   //$(this).removeClass('droidetestsHover');
 
+				   $(this).removeAttr('droideclasshover');
+				   
+				  // $(this).css({'border':'2px solid #ff0000'});
 
 	      	 }
 	      		
@@ -70,6 +83,133 @@
 	      		//console.log($(this));
 	      }); 
 
+		
+		  function verificandoElementoClicado(elemento)
+		  {
+				getseletor = '';
+
+				if(typeof elemento.attr('id') != 'undefined')
+				{
+					this.atributos.seletor = '#'+elemento.attr('id');
+				}else{
+					rasterizebeforetag();
+				}
+
+				analiseSelector();
+				
+				console.log(this.atributos);
+		  }
+
+		  function analiseSelector()
+		  {
+			var patt = new RegExp(":eq\(\d{1,10000}\)",'g');
+
+			if(patt.test(this.atributos.seletor ))
+			{
+				console.log('agora entrou o regex');
+
+				var repseletor = str.replace(patt, "");
+				var getEQ = patt.exec(this.atributos.seletor );
+				this.atributos.seletor = repseletor;
+				this.atributos.eq = getEQ;
+
+				return ;
+			}
+
+			if($(this.atributos.seletor ).length != 1)
+			{
+				itens = [];
+				$.each($(this.atributos.seletor ), function(i,e){
+					if($(this).text() != '')
+					{
+						itens.push('index ['+i+'] valor = '+$(this).text());
+					}else{
+						itens.push('index ['+i+'] valor = icone ou imagem');
+					}
+				});
+
+				var getseletor = prompt("O seletor não é unico, ou não foi encontrado, mude para uma construção unica. caso não seja possível unificar o seletor use :eq(index). Encontrado:\n"+itens.join('\n'), this.atributos.seletor );
+					if(getseletor != null)
+					{
+						this.atributos.seletor = String(getseletor);
+						analiseSelector();
+					}
+					
+
+			}else{
+
+				if($(seletor).length == 1)
+				{
+					this.atributos.seletor = seletor;
+				}
+				
+				
+			}
+
+		  }
+
+		  function rasterizebeforetag(codeSeletor='')
+		  {
+
+			if($(elemento).prop('tagName') =='BODY')
+			{
+				this.atributos.seletor = 'BODY'+codeSeletor;
+			}
+
+			if(typeof elemento.attr('class') != 'undefined')
+			{
+				firstClass = elemento.attr('class').split(' ')[0];
+
+				if(firstClass != '' && $('.'+firstClass).length == 1)
+				{
+
+					testuniqueelements = $('.'+firstClass+codeSeletor);
+
+					if(testuniqueelements.length == 1)
+					{
+						return '.'+firstClass+codeSeletor;
+					}else{
+						codeSeletor = '.'+firstClass+codeSeletor;
+						return rasterizebeforetag(elemento.parent(), " "+codeSeletor);
+					}
+
+					
+				}else if(firstClass != '' &&  $('.'+firstClass).length > 1)
+				{
+					codeSeletor = '.'+firstClass+codeSeletor;
+				}
+			}
+
+				if($(elemento.prop("tagName")).length == 1)
+				{
+
+					testuniqueelements = $(elemento.prop("tagName")+codeSeletor);
+
+					if(testuniqueelements.length == 1)
+					{
+						return elemento.prop("tagName")+codeSeletor;
+					}else{
+						codeSeletor = elemento.prop("tagName")+codeSeletor;
+						return rasterizebeforetag(elemento.parent(), " "+codeSeletor);
+					}
+
+					
+
+				}else if($(elemento.prop("tagName")).length > 1)
+				{
+					codeSeletor = elemento.prop("tagName")+codeSeletor;
+				}
+
+				
+
+				if(typeof elemento.parent() == "object" && elemento.parent().length == 1)
+				{
+					return rasterizebeforetag(elemento.parent(), " "+codeSeletor);
+				}
+				
+
+
+		  }
 
 	      function menu()
 	      {
